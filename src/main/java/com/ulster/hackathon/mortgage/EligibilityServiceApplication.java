6b91c49512d2484a9c27666ee5ed7501a573a430
Stelios Gerogiannakis <sgerogia@gmail.com>
2017-02-18 10:34:11 +0000

    Initial commit

---
+++ src/main/java/com/ulster/hackathon/mortgage/EligibilityServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ulster.hackathon.mortgage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EligibilityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EligibilityServiceApplication.class, args);
+	}
+}
